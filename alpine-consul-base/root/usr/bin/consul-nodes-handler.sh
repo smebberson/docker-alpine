6f905d885ff56cf001dd111c4e999af5d2d32b68
Matthew Valimaki <matthew.valimaki@gmail.com>
2016-01-08 07:36:07 -0800

    Overall improvements
    Signed-off-by: Matthew Valimaki <matthew.valimaki@gmail.com>

--- alpine-consul-base/root/usr/bin/consul-nodes-handler.sh
+++ alpine-consul-base/root/usr/bin/consul-nodes-handler.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
 
+read cat > /dev/null
+
 # count child directories
 # see: http://stackoverflow.com/a/6781464
 # see: https://github.com/smebberson/docker-alpine/pull/13#issuecomment-161005924
 
-# 0) count child directories
+# count child directories
 num_child=`ls -l /etc/consul-template/templates/ | grep -c ^d`
 
-# 1) check if templates exist
+# check if templates exist
 if [ $num_child -gt 0 ]; then
-  # 2) remove `down` to enable service
+  # rm symlink
+  rm /etc/consul/conf.d/watch-node.json
+  
+  # reload consul configuration to refresh watches
+  consul reload
+
+  # remove `down` to enable `consul-down` service
   if [ -e "/var/run/s6/services/consul-template/down" ]; then
     rm /var/run/s6/services/consul-template/down
   fi
   
-  # 3) Execute `s6-svc -u` to notify supervisor that service should start
+  # execute `s6-svc -u` to notify supervisor that `consul-template` service should start
   s6-svc -u /var/run/s6/services/consul-template
-  
-  # 4) rm symlink
-  rm /etc/consul/conf.d/_watch-node.conf
-  
-  # 5) reload consul configuration
-  consul reload
 fi
